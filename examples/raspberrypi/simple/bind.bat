9d5d46fbb32465678151f1d7d0350a62fa9360a1
FotaHub <>
2022-01-12 16:00:33 +0100

    FotaHub Device SDK for Yocto 1.0.0-99

--- examples/raspberrypi/simple/bind.bat
+++ examples/raspberrypi/simple/bind.bat
@@ -4,11 +4,11 @@ setlocal
 rem Make sure that 'fotahub-yocto-factory' container's time syncs with that of host
 wsl -d docker-desktop -e /sbin/hwclock -s
 
-docker volume create fotahub-yocto-factory-data
+docker volume create fotahub-yocto-factory-data-rpi
 docker run ^
-  --name fotahub-yocto-factory ^
+  --name fotahub-yocto-factory-rpi ^
   --interactive --tty --rm ^
-  --volume fotahub-yocto-factory-data:/yocto ^
+  --volume fotahub-yocto-factory-data-rpi:/yocto ^
   --volume %~dp0:/yocto/cockpit ^
   fotahub/yocto-factory:2021.1.0 ^
   %*
