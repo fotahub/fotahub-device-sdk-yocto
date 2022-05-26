dcb0140a7fffbaf347ce3877581d85e87557fe27
FotaHub <>
2022-05-26 07:14:05 +0200

    FotaHub Device SDK for Yocto 1.0.0-108

--- fotahubclient/app_updater.py
+++ fotahubclient/app_updater.py
@@ -17,29 +17,39 @@ class AppUpdater(object):
         self.logger = logging.getLogger()
 
         repo = self.__open_ostree_repo(ostree_repo_path)
-        self.ostree_repo = OSTreeRepo(repo)
-        
-        self.remote_name = self.ostree_repo.guess_remote_name(constants.FOTAHUB_OSTREE_REMOTE_NAME_DEFAULT)
-        self.ostree_repo.add_ostree_remote(self.remote_name, constants.FOTAHUB_OSTREE_REMOTE_URL, ostree_gpg_verify)
+        if repo:
+            self.ostree_repo = OSTreeRepo(repo)
+            
+            self.remote_name = self.ostree_repo.guess_remote_name(constants.FOTAHUB_OSTREE_REMOTE_NAME_DEFAULT)
+            self.ostree_repo.add_ostree_remote(self.remote_name, constants.FOTAHUB_OSTREE_REMOTE_URL, ostree_gpg_verify)
+        else:
+            self.ostree_repo = None
+            self.remote_name = None
 
     def __open_ostree_repo(self, repo_path):
         try:
             repo = OSTree.Repo.new(Gio.File.new_for_path(repo_path))
-            if os.path.exists(repo_path):
-                self.logger.debug("Opening application OSTree repo located at '{}'".format(repo_path))
-                repo.open(None)
-            else:
-                self.logger.debug("Creating and opening new application OSTree repo located at '{}'".format(repo_path))
-                repo.create(OSTree.RepoMode.BARE_USER_ONLY, None)
+            if not os.path.exists(repo_path):
+                self.logger.debug("Application OSTree repo located at '{}' does not exist".format(repo_path))
+                return None
+
+            self.logger.debug("Opening application OSTree repo located at '{}'".format(repo_path))
+            repo.open(None)
             return repo
         except GLib.Error as err:
             raise OSTreeError('Failed to open application OSTree repo') from err
 
     def list_app_names(self):
+        if not self.ostree_repo:
+            return []
+        
         refs = self.ostree_repo.list_ostree_refs()
         return [ref.split(':')[1] if ':' in ref else ref for ref in refs.keys()]
 
     def get_app_deploy_revision(self, name):
+        if not self.ostree_repo:
+            return None
+
         return self.ostree_repo.resolve_ostree_revision(self.remote_name, name)
 
     def get_app_rollback_revision(self, name, deployed_artifacts_path):
@@ -53,11 +63,15 @@ class AppUpdater(object):
 
     def pull_app_update(self, name, revision):
         self.logger.info("Pulling '{}' application revision '{}'".format(name, revision))
+        if not self.ostree_repo:
+            raise OSTreeError("Applications side loading operations are not supported on this system (no application OSTree repo available)")
 
         self.ostree_repo.pull_ostree_revision(self.remote_name, name, revision, constants.OSTREE_PULL_DEPTH)
 
     def checkout_app_revision(self, name, revision, checkout_path):
         self.logger.info("Checking out '{}' application revision '{}'".format(name, revision))
+        if not self.ostree_repo:
+            raise OSTreeError("Applications side loading operations are not supported on this system (no application OSTree repo available)")
 
         try:
             if os.path.isdir(checkout_path):
